8035dac62d7384994e73845b2d7957233ad82a25
Sumit-Autade <sumitautade100@gmail.com>
2025-04-19 10:16:17 +0530

    all the changes

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def sub(a, b):
 def mul(a, b):
     return a*b
 
+if __name__ == "__main__":
+    result1 = add(5, 4)
+    result2 = sub(5, 4)
 
-result1 = add(5, 4)
-result2 = sub(5, 4)
-
-print(result1, result2)
+    print(result1, result2)
