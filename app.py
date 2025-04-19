076900b9db2642ee545be6f052e757804dea5f32
Sumit-Autade <sumitautade100@gmail.com>
2025-04-19 09:06:59 +0530

    fixed the changes

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
 
+def sub(a, b):
+    return a-b
 
-
-
-
-def sub(a,b):return a-b
 
 result1 = add(5,4)
 result2 = sub(5,4)
 
-print(result1)
-print(result2)
+print(result1, result2)
